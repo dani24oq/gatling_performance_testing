4208b49a8f7e802e9aa0ef8036583abe2b10a905
dani24oq <cdanielo@gmail.com>
2023-05-04 12:13:46 -0500

    Custom feeders

--- src/test/java/videogamedb/feeders/VideoGameDbFeeders.java
+++ src/test/java/videogamedb/feeders/VideoGameDbFeeders.java
@@ -5,6 +5,10 @@ import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
@@ -18,11 +22,17 @@ public class VideoGameDbFeeders extends Simulation {
 
     private static FeederBuilder.FileBased<Object> jsonFeeder = jsonFile("data/gameJsonFile.json").random();
 
+    private static Iterator<Map<String, Object>> customFeeder =
+            Stream.generate((Supplier<Map<String, Object>>) () -> {
+                Random rand = new Random();
+                int gameId = rand.nextInt(10 - 1 + 1) + 1;
+                return Collections.singletonMap("gameId", gameId);
+            }).iterator();
+
     private static ChainBuilder getSpecificGame =
-            feed(jsonFeeder)
-                    .exec(http("Get specific game with name - #{name}")
-                    .get("/videogame/#{id}")
-                            .check(jmesPath("name").isEL("#{name}")));
+            feed(customFeeder)
+                    .exec(http("Get specific game with id - #{gameId}")
+                    .get("/videogame/#{gameId}"));
 
     private ScenarioBuilder scn = scenario("Video Game Db - Section 6 code")
             .repeat(10).on(
